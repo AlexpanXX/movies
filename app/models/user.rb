7a81d22fc4ec903e18a16f01749cce8bd2d456aa
AlexPan <ppgod@live.cn>
2017-03-26 12:28:19 +0800

    implement is_follower_of?(film) favorite!(film) dislikes!(film) functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ApplicationRecord
   has_many :films
   has_many :film_relationships
   has_many :favorited_films, through: :film_relationships, source: :film
+
+  def is_follower_of?(film)
+    favorited_films.include?(film)
+  end
+
+  def favorite!(film)
+    favorited_films << film
+  end
+
+  def dislikes!(film)
+    favorited_films.delete(film)
+  end
 end
